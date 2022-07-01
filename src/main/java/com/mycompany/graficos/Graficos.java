d42b74db9fc3e9eba75073bf3bb6df6e05da71e9
Dremi7w7 <Dremi7w7@DESKTOP-73SMQEU>
2022-07-01 07:17:50 -0500

    Ejercicio_3_06

---
+++ src/main/java/com/mycompany/graficos/Graficos.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.graficos;
+
+/**
+ *
+ * @author Dremi7w7
+ */
+public class Graficos {
+
+    public static void main(String[] args) {
+        Ventana miFormulario = new Ventana();
+        miFormulario.setVisible(true); 
+    }
+}
